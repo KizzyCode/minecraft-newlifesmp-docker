0f89c082e81829ec9f28e19b3f310332a794f07a
Keziah Biermann <development@kizzycode.de>
2023-07-29 15:13:12 +0200

    Configurable JVM arguments

--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@ set -eu
 
 # Go into minecraft dir
 cd "/var/minecraft"
-exec java -Xmx6G -jar fabric-server-mc.1.19.2-loader.0.14.21-launcher.0.11.2.jar nogui
+echo "java ${JAVA_ARGS:-} -jar fabric-server-mc.1.19.2-loader.0.14.21-launcher.0.11.2.jar nogui"
+exec java ${JAVA_ARGS:-} -jar fabric-server-mc.1.19.2-loader.0.14.21-launcher.0.11.2.jar nogui
